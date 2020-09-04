c94d934acebae556dffebf43feec8f3ce5bb5df2
Davis <davish225@gmail.com>
2020-09-04 13:23:12 -0400

    fixed paths for images

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,23 @@
-// Get the modal
+// get id of form card
 var modal = document.getElementById('card');
 
-// Get the button that opens the modal
+// id of the button that triggers the card
 var btn = document.getElementById('contact-btn');
 
-// Get the <span> element that closes the modal
+// Get span that closes card
 var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks the button, open the modal
+// When the user clicks the button, open the card
 btn.onclick = function() {
 	modal.style.display = 'block';
 };
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on X, close card
 span.onclick = function() {
 	modal.style.display = 'none';
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks outside the card, close the card
 window.onclick = function(event) {
 	if (event.target == modal) {
 		modal.style.display = 'none';
